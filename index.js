ab7e447ab05a7e2569c7d5b7b62d126d84f8df11
Hossam630 <68388732+Hossam630@users.noreply.github.com>
2023-09-14 20:30:28 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let counter =0;
+function signUpBtnfcn(){
+let messagesList = ['oh you touch my talala!','mmm my ding ding dong.'];
+(counter==messagesList.length)?counter=0:counter=counter;
+window.alert(messagesList[counter]);
+counter++;  
+}
